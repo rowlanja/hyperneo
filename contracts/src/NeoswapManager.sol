d85f667da179f020030c56fa5b7fc38e0b136e92
j r <rowlanja@tcd.ie>
2025-03-27 20:27:07 +0000

    [feat] slippage protection

--- contracts/src/NeoswapManager.sol
+++ contracts/src/NeoswapManager.sol
@@ -4,20 +4,54 @@ import "./NeoswapPool.sol";
 import "./interfaces/IERC20.sol";
 
 contract NeoswapManager {
+
+    struct MintParams {
+        address poolAddress;
+        int24 lowerTick;
+        int24 upperTick;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+    }
+
     function mint(
-        address poolAddress_,
-        int24 lowerTick,
-        int24 upperTick,
-        uint128 liquidity,
-        bytes calldata data
-    ) public {
-        NeoswapPool(poolAddress_).mint(
-            msg.sender,
-            lowerTick,
-            upperTick,
-            liquidity,
-            data
+        MintParams calldata params
+    ) public  (uint256 amount0, uint256 amount1) {
+        IUniswapV3Pool pool = IUniswapV3Pool(params.poolAddress);
+
+        (uint160 sqrtPriceX96, ) = pool.slot0();
+        uint160 sqrtPriceLowerX96 = TickMath.getSqrtRatioAtTick(
+            params.lowerTick
         );
+        uint160 sqrtPriceUpperX96 = TickMath.getSqrtRatioAtTick(
+            params.upperTick
+        );
+
+        uint128 liquidity = LiquidityMath.getLiquidityForAmounts(
+            sqrtPriceX96,
+            sqrtPriceLowerX96,
+            sqrtPriceUpperX96,
+            params.amount0Desired,
+            params.amount1Desired
+        );
+
+        (amount0, amount1) = pool.mint(
+            msg.sender,
+            params.lowerTick,
+            params.upperTick,
+            liquidity,
+            abi.encode(
+                IUniswapV3Pool.CallbackData({
+                    token0: pool.token0(),
+                    token1: pool.token1(),
+                    payer: msg.sender
+                })
+            )
+        );
+
+        if (amount0 < params.amount0Min || amount1 < params.amount1Min)
+            revert SlippageCheckFailed(amount0, amount1);
     }
 
     // function swap(
---
+++ goals.md
@@ -0,0 +1,13 @@
+Goals for week 24th may
+
+-> work through https://uniswapv3book.com/milestone_3/introduction.html
+
+-> 
+update the mint function to provide liquidity in different price ranges;
+update the swap function to cross price ranges when there’s not enough liquidity in the current price range;
+learn how to calculate liquidity in smart contracts;
+implement slippage protection in the mint and swap functions;
+update the UI application to allow to add liquidity at different price ranges;
+learn a little bit more about fixed-point numbers.
+
+-> deploy on hyperliquid
