916a024e5a9e79f1429a606dbaef5c8e5f7133db
j r <rowlanja@tcd.ie>
2025-04-07 11:55:39 +0100

    [shuffle]

--- contracts/src/interfaces/IUniswapV3MintCallback.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0;
-
-interface IUniswapV3MintCallback {
-    function uniswapV3MintCallback(uint256 amount0, uint256 amount1, bytes calldata data) external;
-}
